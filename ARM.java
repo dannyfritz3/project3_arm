bf2a7bcc435e1e0c5b616d3c131b2f80028e616a
Danny <drfritz@csbsju.edu>
2018-04-05 13:35:49 -0500

    quick fix

--- ARM.java
+++ ARM.java
@@ -8,13 +8,18 @@ public class ARM {
         if(minsupp < 0 || minsupp > 100) {
             throw new IndexOutOfBoundsException("Please select a minsupp threshold between 0 and 100");
         }
-
         try {
             Scanner scAccidents = new Scanner("datasets/accidents.data");
             Scanner scChess = new Scanner("datasets/chess.data");
             Scanner scKosarak = new Scanner("datasets/kosarak.data");
             Scanner scRetail = new Scanner("datasets/retail.data");
             Scanner scSimple = new Scanner("datasets/simpledataset.data");
+
+            scAccidents.close();
+            scChess.close();
+            scKosarak.close();
+            scRetail.close();
+            scSimple.close();
         } catch(Exception e) {
             System.out.println(e);
         }
