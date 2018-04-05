ec23fb52151a20ff232be4d184c52fd941f72e24
Danny <drfritz@csbsju.edu>
2018-04-05 13:32:41 -0500

    Initital setup as well as Scanners setup

---
+++ ARM.java
@@ -0,0 +1,22 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
+
+public class ARM {
+    public static void main(String [] args) throws IndexOutOfBoundsException, FileNotFoundException {
+        double minsupp = Double.parseDouble(args[0]);
+        if(minsupp < 0 || minsupp > 100) {
+            throw new IndexOutOfBoundsException("Please select a minsupp threshold between 0 and 100");
+        }
+
+        try {
+            Scanner scAccidents = new Scanner("datasets/accidents.data");
+            Scanner scChess = new Scanner("datasets/chess.data");
+            Scanner scKosarak = new Scanner("datasets/kosarak.data");
+            Scanner scRetail = new Scanner("datasets/retail.data");
+            Scanner scSimple = new Scanner("datasets/simpledataset.data");
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+}
