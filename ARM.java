0e4542bb030b87ae6f331582d21a635c4b04ba9e
Conor Lorsung <cglorsung@gmail.com>
2018-04-11 14:44:57 -0500

    Added prefix check. Now get proper sets.

--- ARM.java
+++ ARM.java
@@ -1,3 +1,4 @@
+
 /** 
  * Authors: Danny Fritz and Conor Lorsung
  * Purpose: Apriori algorithm for science and a good grade.
@@ -8,17 +9,16 @@ import java.io.IOException;
 import java.util.*;
 import java.io.*;
 
-
 public class ARM {
 
-    private static ArrayList<int []> transactionsList;
+    private static ArrayList<int[]> transactionsList;
     private static int minsupp;
     private static int numItems;
     private static int numTransactions;
 
-    public static int [] generateFrequentItemSubset(int k) {
-        int [] L1 = new int[0];
-        for(int i = 1; i <= numItems; i++) {
+    public static int[] generateFrequentItemSubset(int k) {
+        int[] L1 = new int[0];
+        for (int i = 1; i <= numItems; i++) {
             L1[i] = i;
         }
         return null;
@@ -27,13 +27,13 @@ public class ARM {
     private static void apriori(int size, ArrayList<int[]> input) {
         //Variables used in the method
         ArrayList<ArrayList<Integer>> ck = new ArrayList<ArrayList<Integer>>();
-        ArrayList<ArrayList<Integer>> lk  = new ArrayList<ArrayList<Integer>>();
+        ArrayList<ArrayList<Integer>> lk = new ArrayList<ArrayList<Integer>>();
         ArrayList<ArrayList<Integer>> l1 = new ArrayList<ArrayList<Integer>>();
-        ArrayList<Transaction>        at = new ArrayList<Transaction>();
-        ArrayList<Transaction>        ft = new ArrayList<Transaction>();
+        ArrayList<Transaction> at = new ArrayList<Transaction>();
+        ArrayList<Transaction> ft = new ArrayList<Transaction>();
 
         //Build transactions for each int[] in the input list
-        for(int[] i : input) {
+        for (int[] i : input) {
             at.add(new Transaction(i, 0));
         }
 
@@ -41,9 +41,9 @@ public class ARM {
         ArrayList<Integer> values = new ArrayList<Integer>();
 
         //Parse out the unique values and add them to ArrayList values
-        for(Transaction t : at) {
-            for(int i = 0; i < t.getItems().length; i++) {
-                if(!values.contains(t.getItems()[i])) {
+        for (Transaction t : at) {
+            for (int i = 0; i < t.getItems().length; i++) {
+                if (!values.contains(t.getItems()[i])) {
                     values.add(t.getItems()[i]);
                 }
             }
@@ -51,10 +51,10 @@ public class ARM {
 
         //Sort the ArrayList for fun
         Collections.sort(values);
-        
+
         //Create new transactions for each of the unique values
-        for(int i : values) {
-            ft.add(new Transaction(new int[]{i}, 0));
+        for (int i : values) {
+            ft.add(new Transaction(new int[] { i }, 0));
         }
 
         //Generate updated support weights for the transactions
@@ -62,26 +62,26 @@ public class ARM {
 
         //Debugging print after generated weights
         System.out.println("------------");
-        for(Transaction t : ft) {
+        for (Transaction t : ft) {
             System.out.println("FT: " + t.toString());
         }
 
-        ft = updateSets(ft, 0.6, 2);
+        ft = updateSets(ft, 0.5, 2);
         //Debugging print after update
         System.out.println("---update1---");
-        for(Transaction t : ft) {
+        for (Transaction t : ft) {
             System.out.println("FT: " + t.toString());
         }
 
         ft = genWeights(input, ft);
         //Debugging print after update
         System.out.println("---update2---");
-        for(Transaction t : ft) {
+        for (Transaction t : ft) {
             System.out.println("FT: " + t.toString());
         }
 
-        for(int i = 0; i < ft.size(); i++) {
-            for(int j = i+1; j < ft.size(); j++) {
+        for (int i = 0; i < ft.size(); i++) {
+            for (int j = i + 1; j < ft.size(); j++) {
                 System.out.println(Arrays.toString(setUnion(ft, ft.get(i).getItems(), ft.get(j).getItems())));
             }
         }
@@ -90,7 +90,7 @@ public class ARM {
         int[] vals1 = setUnion(ft, ft.get(0).getItems(), ft.get(1).getItems());
         int[] vals2 = setUnion(ft, ft.get(0).getItems(), ft.get(2).getItems());
         int[] vals3 = setUnion(ft, vals1, vals2);
-
+        
         //Debugging print
         System.out.println("V1: " + Arrays.toString(vals1) + "\nV2: " + Arrays.toString(vals2) + "\nV3: " + Arrays.toString(vals3));
         */
@@ -107,41 +107,41 @@ public class ARM {
      * <ArrayList input>.
      */
     private static ArrayList<Transaction> genWeights(ArrayList<int[]> input, ArrayList<Transaction> data) {
-        for(Transaction t : data) {
+        for (Transaction t : data) {
             double supp = 0.0;
-            int    suppCount = 0;
+            int suppCount = 0;
 
-            for(int[] arr : input) {
+            for (int[] arr : input) {
 
-                int[] loopArr  = new int[]{};
-                int[] checkArr = new int[]{};
+                int[] loopArr = new int[] {};
+                int[] checkArr = new int[] {};
 
                 boolean flip = true;
 
-                if(t.getItems().length >= arr.length) {
+                if (t.getItems().length >= arr.length) {
                     checkArr = t.getItems();
                     loopArr = arr;
                     flip = false;
-                } else if(t.getItems().length < arr.length) {
+                } else if (t.getItems().length < arr.length) {
                     checkArr = arr;
                     loopArr = t.getItems();
                     flip = true;
                 }
 
                 int check = 0;
-                for(int i = 0; i < loopArr.length; i++) {
-                    for(int j = 0; j < checkArr.length; j++) {
-                        if(loopArr[i] == checkArr[j]) {
+                for (int i = 0; i < loopArr.length; i++) {
+                    for (int j = 0; j < checkArr.length; j++) {
+                        if (loopArr[i] == checkArr[j]) {
                             check++;
                         }
                     }
                 }
-                if(flip) {
-                    if(check == loopArr.length) {
+                if (flip) {
+                    if (check == loopArr.length) {
                         suppCount++;
                     }
                 } else {
-                    if(check == checkArr.length) {
+                    if (check == checkArr.length) {
                         suppCount++;
                     }
                 }
@@ -152,19 +152,19 @@ public class ARM {
     }
 
     private static ArrayList<Transaction> updateSets(ArrayList<Transaction> data, double threshold, int k) {
-        for(int i = 0; i < data.size(); i++) {
-            if(data.get(i).getSupport() < threshold) {
+        for (int i = 0; i < data.size(); i++) {
+            if (data.get(i).getSupport() < threshold) {
                 data.remove(i);
             }
         }
-        
+
         ArrayList<Transaction> arrTransaction = new ArrayList<Transaction>();
 
-        int [] group = new int[]{};
+        int[] group = new int[] {};
 
-        for(int i = 0; i < data.size(); i++) {
-            for(int j = i + 1; j < data.size(); j++) {
-                group = new int [k];
+        for (int i = 0; i < data.size(); i++) {
+            for (int j = i + 1; j < data.size(); j++) {
+                group = new int[k];
                 group[0] = data.get(i).getItems()[0];
                 group[1] = data.get(j).getItems()[0];
                 Transaction t = new Transaction(group, threshold);
@@ -174,10 +174,10 @@ public class ARM {
         return arrTransaction;
     }
 
-    public static void main(String [] args) throws IndexOutOfBoundsException, FileNotFoundException {
+    public static void main(String[] args) throws IndexOutOfBoundsException, FileNotFoundException {
         //double minsupp = Double.parseDouble(args[0]);
         minsupp = 5;
-        if(minsupp < 0 || minsupp > 100) {
+        if (minsupp < 0 || minsupp > 100) {
             throw new IndexOutOfBoundsException("Please select a minsupp threshold between 0 and 100");
         }
         try {
@@ -187,17 +187,17 @@ public class ARM {
             Scanner scRetail = new Scanner(new File("datasets/retail.dat"));
             Scanner scSimple = new Scanner(new File("datasets/simpledataset.dat"));
 
-            transactionsList = new ArrayList<int []>();
+            transactionsList = new ArrayList<int[]>();
 
             //reassign sc for the data file you want to scan
             Scanner sc = scSimple;
 
-            while(sc.hasNextLine()) {
+            while (sc.hasNextLine()) {
                 String line = sc.nextLine();
-                String [] strArr = line.split(" ");
-                int [] transaction = new int[strArr.length];
+                String[] strArr = line.split(" ");
+                int[] transaction = new int[strArr.length];
 
-                for(int i = 0; i < strArr.length; i++) {
+                for (int i = 0; i < strArr.length; i++) {
                     transaction[i] = Integer.parseInt(strArr[i]);
                 }
 
@@ -212,7 +212,7 @@ public class ARM {
 
             apriori(5, transactionsList);
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
@@ -225,51 +225,59 @@ public class ARM {
         ArrayList<Integer> prefLocs = new ArrayList<Integer>();
 
         //Iterate through the transaction data and parse out the prefixes
-        for(int i = 0; i < trans.size(); i++) {
+        for (int i = 0; i < trans.size(); i++) {
 
             //Get the prefixes of the current transactions
-            int[] prefArr = Arrays.copyOfRange(trans.get(i).getItems(), 0, trans.get(i).getItems().length-1);
+            //int[] prefArr = Arrays.copyOfRange(trans.get(i).getItems(), 0, trans.get(i).getItems().length-1);
             //System.out.println("PREFIX ARR: " + Arrays.toString(prefArr));
 
             //Add prefixes and their locations to prefixes ArrayList and prefLocs ArrayList respectively
-            for(int j = 0; j < trans.get(i).getItems().length-1; j++) {
+            for (int j = 0; j < trans.get(i).getItems().length - 1; j++) {
                 Integer prefix = trans.get(i).getItems()[j];
-                if(!prefixes.contains(prefix)) {
+                if (!prefixes.contains(prefix)) {
                     prefixes.add(prefix);
                     prefLocs.add(i);
                 }
             }
         }
-        
+
         //System.out.println(prefixes + "\n" + prefLocs);
-        System.out.println("U: " + Arrays.toString(x) + " : " + Arrays.toString(y));
-        Integer[] opX = new Integer[x.length];
-        for(int i = 0; i < x.length; i++) {
-            opX[i] = x[i];
-        }
 
-        Integer[] opY = new Integer[y.length];
-        for(int i = 0; i < y.length; i++) {
-            opY[i] = y[i];
-        }
 
-        //Union the arrays x and y
-        Set<Integer> newSet = new HashSet<Integer>(Arrays.asList(opX));
-        newSet.addAll(Arrays.asList(opY));
-        Integer[] opArr = newSet.toArray(new Integer[newSet.size()]);
+        if (Arrays.equals(getPrefix(x), getPrefix(y))) {
 
-        int[] outArr = new int[opArr.length];
-        for(int i = 0; i < opArr.length; i++) {
-            outArr[i] = opArr[i];
+            System.out.println("U: " + "PRE: " + Arrays.toString(getPrefix(x)) + " | " + Arrays.toString(x) + " : PRE: "
+            + Arrays.toString(getPrefix(y)) + " | " + Arrays.toString(y));
+
+            Integer[] opX = new Integer[x.length];
+            for (int i = 0; i < x.length; i++) {
+                opX[i] = x[i];
+            }
+
+            Integer[] opY = new Integer[y.length];
+            for (int i = 0; i < y.length; i++) {
+                opY[i] = y[i];
+            }
+
+            //Union the arrays x and y
+            Set<Integer> newSet = new HashSet<Integer>(Arrays.asList(opX));
+            newSet.addAll(Arrays.asList(opY));
+            Integer[] opArr = newSet.toArray(new Integer[newSet.size()]);
+
+            int[] outArr = new int[opArr.length];
+            for (int i = 0; i < opArr.length; i++) {
+                outArr[i] = opArr[i];
+            }
+            return outArr;
         }
-        return outArr;
+        return new int[]{-1};
     }
 
-    public static int [] getPrefix(int [] arr) {
+    public static int[] getPrefix(int[] arr) {
         int size = arr.length;
-        int [] newArr = new int[size-1];
-        
-        for(int i = 0; i < size - 2; i++) {
+        int[] newArr = new int[size - 1];
+
+        for (int i = 0; i < size - 1; i++) {
             newArr[i] = arr[i];
         }
         return newArr;
@@ -278,7 +286,7 @@ public class ARM {
     //Transaction object that stores an integer array of values and a transaction support value
     static class Transaction {
 
-        private int[]  items;
+        private int[] items;
         private double support;
 
         public Transaction(int[] items, double support) {
